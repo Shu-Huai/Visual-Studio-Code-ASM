2c40075086fa79d66507656b326d39697f2e4b2d
殊怀丶 <lvzhihe_123@qq.com>
2021-09-15 16:24:23 +0800

    changed my 打印所有字符

--- 打印所有字符.asm
+++ 打印所有字符.asm
@@ -1,31 +1,34 @@
 CODE SEGMENT
-	      ASSUME CS:CODE
-	START:
-	      MOV    DX,0
-	      MOV    DL,10H
-	      MOV    CX,0FH
-	      MOV    AX,0
-	R:    
-	      MOV    AH,02
-	      PUSH   CX
-	      MOV    CX,10H
-	Z:    
-	      INT    21H
-	      PUSH   DX
-	      MOV    DL,0
-	      INT    21H
-	      POP    DX
-	      INC    DX
-	      LOOP   Z
-	      POP    CX
-	      PUSH   DX
-	      MOV    DL,0DH
-	      INT    21H
-	      MOV    DL,0AH
-	      INT    21H
-	      POP    DX
-	      LOOP   R
-	      MOV    AH,4CH
-	      INT    21H
+	       ASSUME CS:CODE
+	START: 
+	       MOV    DL,0FH
+	       MOV    AH,2H
+	       MOV    CL,10H
+	       MOV    CH,0FH
+	PRINT: 
+	       INC    DL
+	       INT    21H
+	       MOV    BL,DL
+	       MOV    DL,0H
+	       INT    21H
+	       MOV    DL,BL
+	       MOV    AL,0H
+	       DEC    CL
+	       JZ     RETURN
+	       JMP    PRINT
+	RETURN:
+	       MOV    CL,10H
+	       MOV    DH,DL
+	       MOV    DL,0DH
+	       INT    21H
+	       MOV    DL,0AH
+	       INT    21H
+	       MOV    DL,DH
+	       DEC    CH
+	       JZ     EXIT
+	       JMP    PRINT
+	EXIT:    
+	       MOV    AH,4CH
+	       INT    21H
 CODE ENDS  
 END START 
